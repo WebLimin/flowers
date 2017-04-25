69e0ea98cee9a047d2e1bb8f08bc66b724c39273
WebLimin <464004210@qq.com>
2017-04-25 08:13:35 +0800

    commit message

--- js/flower.js
+++ js/flower.js
@@ -116,10 +116,12 @@ app.controller('detailCtrl', ['$scope', '$fHttp', '$stateParams', '$ionicPopup',
                 $scope.f = data[0];
             }
         );
-
+        sessionStorage.did=[];
+        sessionStorage.did=$stateParams.id;
+        console.log(sessionStorage.did);
         $scope.addToCart = function () {
             $fHttp.sendRequest(
-                'data/cart_update.php?uid=1&did=' + $stateParams.id + "&count=-1",
+                'data/cart_update.php?uid=' + sessionStorage.userId + '&did=' + $stateParams.id + "&count=-1",
                 function (data) {
                     if (data.msg == 'succ') {
                         $ionicPopup.alert({
@@ -127,6 +129,7 @@ app.controller('detailCtrl', ['$scope', '$fHttp', '$stateParams', '$ionicPopup',
                         });
                     }
                 }
+
             )
         }
     }
@@ -134,8 +137,7 @@ app.controller('detailCtrl', ['$scope', '$fHttp', '$stateParams', '$ionicPopup',
 
 app.controller('orderCtrl', ['$scope', '$fHttp', '$stateParams', '$httpParamSerializerJQLike',
     function ($scope, $fHttp, $stateParams, $httpParamSerializerJQLike) {
-        $scope.order = {did: $stateParams.id}
-        console.log($scope.order);
+        $scope.order = {did: sessionStorage.did,userid:sessionStorage.userId};
         $scope.submitOrder = function () {
             var result = $httpParamSerializerJQLike($scope.order);
             console.log(result);
@@ -144,7 +146,7 @@ app.controller('orderCtrl', ['$scope', '$fHttp', '$stateParams', '$httpParamSeri
                 function (data) {
                     if (data.length > 0) {
                         if (data[0].msg == 'succ') {
-                            sessionStorage.setItem('phone', $scope.order.phone)
+                            sessionStorage.setItem('phone', $scope.order.phone);
                             $scope.requestResult = '下单支付成功，订单编号为' + data[0].oid;
                         } else {
                             $scope.requestResult = '下单支付失败';
@@ -155,12 +157,14 @@ app.controller('orderCtrl', ['$scope', '$fHttp', '$stateParams', '$httpParamSeri
         }
     }]);
 app.controller('myOrderCtrl', ['$scope', '$fHttp', function ($scope, $fHttp) {
-    $fHttp.sendRequest(
-        'data/order_getbyuserid.php?userid=1',
-        function (serverData) {
-            $scope.orderList = serverData.data;
-        }
-    )
+    if (sessionStorage.userId) {
+        $fHttp.sendRequest(
+            'data/order_getbyuserid.php?userid=' + sessionStorage.userId,
+            function (serverData) {
+                $scope.orderList = serverData.data;
+            }
+        )
+    }
 }]);
 app.controller('settingCtrl', ['$scope', '$ionicModal',
     function ($scope, $ionicModal) {
@@ -203,7 +207,7 @@ app.controller('myCartCtrl', ['$scope', '$fHttp', function ($scope, $fHttp) {
     }
 
     $fHttp.sendRequest(
-        'data/cart_select.php?uid=1',
+        'data/cart_select.php?uid=' + sessionStorage.userId,
         function (serverDate) {
             $scope.cart = serverDate.data;
         }
@@ -219,7 +223,7 @@ app.controller('myCartCtrl', ['$scope', '$fHttp', function ($scope, $fHttp) {
 
     $scope.updateToServer = function (id, count) {
         $fHttp.sendRequest(
-            'data/cart_update.php?uid=1&did=' + id + "&count=" + count,
+            'data/cart_update.php?uid=' + sessionStorage.userId + '&did=' + id + "&count=" + count,
             function (data) {
 
             }
@@ -227,7 +231,7 @@ app.controller('myCartCtrl', ['$scope', '$fHttp', function ($scope, $fHttp) {
     }
     $scope.deleteToServer = function (id, count) {
         $fHttp.sendRequest(
-            'data/cart_delete.php?uid=1&did=' + id + "&count=" + count,
+            'data/cart_delete.php?uid=' + sessionStorage.userId + '&did=' + id + "&count=" + count,
             function (data) {
             }
         )
@@ -273,11 +277,10 @@ app.controller('myCartCtrl', ['$scope', '$fHttp', function ($scope, $fHttp) {
 
     }
 }]);
-var userId = " ";
-var uName = " ";
+
 /*用户登录部分*/
-app.controller('Login', ['$scope', '$fHttp',
-    function ($scope, $fHttp) {
+app.controller('Login', ['$scope', '$fHttp', '$ionicPopup',
+    function ($scope, $fHttp, $ionicPopup) {
         $scope.ulogin = {uName: "", upwd: ""};
         $scope.$watch('ulogin', function () {
             $scope.uLogin = function () {
@@ -285,12 +288,16 @@ app.controller('Login', ['$scope', '$fHttp',
                     'data/user_login.php?phone=' + $scope.ulogin.uName + "&upwd=" + $scope.ulogin.upwd,
                     function (data) {
                         if (data == -1) {
-                            alert("用户名或密码有误！");
+                            $ionicPopup.alert({
+                                template: "用户名或密码有误！"
+                            });
                         } else {
-                            alert("登录成功！欢迎回来！" + data.uname);
+                            $ionicPopup.alert({
+                                template: "登录成功！欢迎回来！" + data.uname
+                            });
                             sessionStorage.userId = data.userid;
                             sessionStorage.uName = data.uname;
-                            location.href="index.html#/f_main";
+                            location.href = "index.html#/f_main";
                         }
 
                     }
@@ -300,8 +307,8 @@ app.controller('Login', ['$scope', '$fHttp',
     }]);
 
 /*用户注册部分*/
-app.controller('Register', ['$scope', '$fHttp',
-    function ($scope, $fHttp) {
+app.controller('Register', ['$scope', '$fHttp', '$ionicPopup',
+    function ($scope, $fHttp, $ionicPopup) {
         $scope.uregister = {uName: '', uPwd: '', uPhone: ''};
         $scope.$watch('uregister', function () {
             $scope.uRegister = function () {
@@ -309,12 +316,16 @@ app.controller('Register', ['$scope', '$fHttp',
                     'data/user_register.php?uName=' + $scope.uregister.uName +
                     "&uPhone=" + $scope.uregister.uPhone + "&uPwd=" + $scope.uregister.uPwd,
                     function (data) {
-                       if(data=='succ'){
-                           alert('注册成功！');
-                            location.href="index.html#/f_loGin";
-                       }else{
-                           alert('注册失败！手机号码被已注册');
-                       }
+                        if (data == 'succ') {
+                            $ionicPopup.alert({
+                                template: '注册成功！'
+                            });
+                            location.href = "index.html#/f_loGin";
+                        } else {
+                            $ionicPopup.alert({
+                                template: '注册失败！手机号码被已注册'
+                            })
+                        }
                     }
                 )
             }
